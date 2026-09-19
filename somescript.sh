fa4beeafb263ad6421a068f70c30c8bba66974d5
Absolutebob <dimdumpod@gmail.com>
2026-09-19 19:50:20 +0000

    i add some scripts

--- somescript.sh
+++ somescript.sh
@@ -1,1 +1,8 @@
-i want to test something
+#!/usr/bin/bash
+
+cpu_usage=$(top | head -n 3 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2,4)
+mem_usage=$(free | head -n 2 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 3)
+
+echo "Total cpu usage is $cpu_usage"
+echo "Total memory usage is $mem_usage"
+
